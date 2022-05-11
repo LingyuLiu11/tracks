43875cdeb395c7570b411f775b173d103fb0a34f
LingyuLiu11 <linalxly@gmail.com>
2022-05-10 21:36:35 -0400

    initialize

---
+++ src/screens/AccountScreen.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import {View, StyleSheet, Text} from 'react-native';
+
+const AccountScreen = () => {
+    return (
+        <View>account screen</View>
+    )
+}
+
+const styles = StyleSheet.create({});
+
+export default AccountScreen;
---
+++ src/screens/SigninScreen.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import {View, StyleSheet, Text} from 'react-native';
+
+const SigninScreen = () => {
+    return (
+        <View>account screen</View>
+    )
+}
+
+const styles = StyleSheet.create({});
+
+export default SigninScreen;
