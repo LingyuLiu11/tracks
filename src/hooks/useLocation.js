769cef0a37ef4876ed7774924bf54d323134356a
LingyuLiu11 <linalxly@gmail.com>
2022-05-12 22:01:54 -0400

    initialize custom hook useLocation.js

---
+++ src/hooks/useLocation.js
@@ -0,0 +1,7 @@
+import React, {useState, useEffect} from "react";
+
+import { Accuracy, requestForegroundPermissionsAsync, watchPositionAsync } from "expo-location";
+
+export default () => {
+    
+}
