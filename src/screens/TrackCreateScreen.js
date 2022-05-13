4e7fa85fbb36dfe0eb36311b7d9c9ee96b7e02d4
LingyuLiu11 <linalxly@gmail.com>
2022-05-12 21:17:54 -0400

    testing location via _mockLocation.js

--- src/screens/TrackCreateScreen.js
+++ src/screens/TrackCreateScreen.js
@@ -1,9 +1,12 @@
+import '../_mockLocation';
 import React, {useEffect, useState} from "react";
 
 import { View, StyleSheet, Text, Button } from "react-native";
 import Map from "../components/Map";
 import { SafeAreaView } from "react-native";
-import { requestForegroundPermissionsAsync } from "expo-location";
+import { requestForegroundPermissionsAsync, watchPositionAsync, Accuracy } from "expo-location";
+
+
 
 const TrackCreateScreen = ({navigation}) => {
   const [err, setErr] = useState(null);
@@ -11,6 +14,13 @@ const TrackCreateScreen = ({navigation}) => {
   const startWatching = async () => {
     try {
       await requestForegroundPermissionsAsync();
+      await watchPositionAsync({
+        accuracy: Accuracy.BestForNavigation,
+        timeInterval: 1000,
+        distanceInterval: 10
+      }, (location) => {
+        console.log(location);
+      });
     } catch(e) {
       setErr(e);
     }
