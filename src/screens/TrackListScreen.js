2376226b3061e0da18ae59ebe09c58a0e4e072ee
LingyuLiu11 <linalxly@gmail.com>
2022-05-13 23:08:21 -0400

    adding list to show tracks

--- src/screens/TrackListScreen.js
+++ src/screens/TrackListScreen.js
@@ -1,11 +1,47 @@
-import React from "react";
+import React, { useContext, useState } from "react";
 
-import { View, StyleSheet, Text } from "react-native";
+import { View, StyleSheet, Text, Button, FlatList, TouchableOpacity } from "react-native";
 
-const TrackListScreen = () => {
+import { Context as TrackContext } from "../context/TrackContext";
+import { ListItem } from "react-native-elements";
+import { NativeAppEventEmitter } from "react-native";
+
+const TrackListScreen = ({ navigation }) => {
+  const { state, fetchTracks } = useContext(TrackContext);
+  const [ls, setls] = useState([]);
+
+  const setdata = () => {
+    fetchTracks();
+    setls(state);
+    // console.log(state);
+  }
+  
+  
   return (
     <View>
+      
       <Text>Track list screen</Text>
+      <Button
+        title="fetch"
+        onPress={() => setdata()}
+      ></Button>
+      <FlatList date = {ls} 
+        keyExtractor={item => item._id}
+        renderItem={({item}) => {
+          return <TouchableOpacity>
+            <ListItem>
+                <ListItem.Content>
+                  <ListItem.Title>{item.name}</ListItem.Title>
+                </ListItem.Content>
+                <ListItem.Chevron />
+              </ListItem>
+          </TouchableOpacity>
+        }}
+      >
+
+      </FlatList>
+      
+
     </View>
   );
 };
